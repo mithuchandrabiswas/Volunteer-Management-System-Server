cdb727c65f335cf2cb06a9779621b6503112604e
Mithu1424882 <biswasmithumcb@gmail.com>
2024-05-10 19:46:15 +0600

    apply a custom middleware for token verify

--- index.js
+++ index.js
@@ -22,6 +22,28 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+//Created Middleware
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+    // console.log(token);
+    if (!token) {
+        return res.status(401).send({ message: 'unathorized access' });
+    }
+    if (token) {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, docoded) => {
+            if (err) {
+                console.log(err);
+                return res.status(401).send({ message: 'unathorized access' });
+            }
+            console.log(docoded);
+            req.user = docoded
+            next();
+        })
+    }
+
+}
+
+
 
 
 
